ab2eece00a88362ee32221a184a9c7a1445ce62e
caiocampos <ccamposs2007@gmail.com>
2025-05-15 11:38:07 -0300

    finalizado

--- site-exemplo/script.js
+++ site-exemplo/script.js
@@ -1,18 +1,14 @@
-//variaveias
+//variaveis
 const apikey = "9aeb86a8d321c1a1ec30bc218dc4dece";
-
 const cityinput = document.getElementById("city-input");
 const searchBtn = document.getElementById("search");
-
 const cityElement = document.querySelector("#city");
 const tempElement = document.querySelector("#temperatura span");
 const descElement = document.querySelector("#description");
 const umiditElement = document.querySelector("#umidade span");
 const windElement = document.querySelector("#vento span");
-
-const containerwather = document.querySelecto("#dados-previsao");
-//funcoes
-
+const containerwather = document.querySelector("#dados-previsao");
+// funcoes
 const getdadostempo = async (city) => {
   const apitempoURL =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -20,12 +16,10 @@ const getdadostempo = async (city) => {
     "&units=metric&appid=" +
     apikey +
     "&lang=pt";
-
   const res = await fetch(apitempoURL);
   const data = await res.json();
   return data;
 };
-
 const showWeatherdata = async (city) => {
   const data = await getdadostempo(city);
   cityElement.innerHTML = data.name;
@@ -33,7 +27,6 @@ const showWeatherdata = async (city) => {
   descElement.innerHTML = data.weather[0].description;
   windElement.innerHTML = data.wind.speed + "km/h";
   umiditElement.innerHTML = data.main.humidity + "%";
-
   containerwather.classList.remove("hide");
 };
 //eventos
